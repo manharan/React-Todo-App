071ed5f4357304985fe07676bd7197c5771539bf
manaharan <manohar1232004@gmail.com>
2016-12-21 22:11:15 +0530

    prep repo for Heroku

--- server.js
+++ server.js
@@ -2,6 +2,16 @@ var express = require('express');
 
 var app = express();
 
+var port = process.env.PORT || 3000;
+
+app.use(function (req, res, next) {
+	if(req.headers['x-forwarded-proto'] === 'http'){
+		next();
+	}else {
+		res.redirect('http://'+req.hostname + req.url);
+	}
+});
+
 app.use(express.static('public'));
 
 app.listen(3000, function () {
